d9919eb4fe52569579b5a3b88183272be5e12b15
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-03-20 15:50:48 +0100

    Add header component with navigation links and styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,21 @@
 export default function Header() {
     return (
         <header>
-            <img src="./dc-logo.png" alt="" />
+            <div className="container">
+                <img src="./dc-logo.png" alt="" />
+                <ul>
+                    <li><a href="">CHARACTERS</a></li>
+                    <li><a href="">COMICS</a></li>
+                    <li><a href="">MOVIES</a></li>
+                    <li><a href="">TV</a></li>
+                    <li><a href="">GAMES</a></li>
+                    <li><a href="">COLLECTIBLES</a></li>
+                    <li><a href="">VIDEOS</a></li>
+                    <li><a href="">FANS</a></li>
+                    <li><a href="">NEWS</a></li>
+                    <li><a href="">SHOP</a></li>
+                </ul>
+            </div>
         </header>
     )
 }
