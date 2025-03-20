eda1d9ed4ec9d0fbad3515c47db1307e62f2e213
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-03-20 15:26:20 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header>
+            <img src="./dc-logo.png" alt="" />
+        </header>
+    )
+}
