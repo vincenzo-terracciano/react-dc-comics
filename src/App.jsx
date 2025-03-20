c8be944ef5890d28f121393cea3175676225838a
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-03-20 16:45:35 +0100

    Add Main component and update App layout with new sections and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Header from './components/Header'
+import Main from './components/Main'
 
 function App() {
 
   return (
     <>
       <Header />
+      <Main />
     </>
   )
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,46 @@
+export default function Main() {
+    return (
+        <main>
+            <section className="dark">
+                <div className="cont">
+                    <h3>-- Content goes here --</h3>
+                </div>
+            </section>
+            <section className="primary">
+
+                <ul>
+                    <li>
+                        <a href="">
+                            <img src="./buy-comics-digital-comics.png" alt="" />
+                            <span>DIGITAL COMICS</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="./buy-comics-merchandise.png" alt="" />
+                            <span>DC MERCHANDISE</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="./buy-comics-subscriptions.png" alt="" />
+                            <span>SUBSCRIPTION</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="./buy-comics-shop-locator.png" className="locator" alt="" />
+                            <span>COMIC SHOP LOCATOR</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="./buy-dc-power-visa.svg" alt="" />
+                            <span>DC POWER VISA</span>
+                        </a>
+                    </li>
+                </ul>
+            </section>
+        </main>
+    )
+}
